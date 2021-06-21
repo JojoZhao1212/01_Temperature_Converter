bc1f0ed363d0c0838d421da76871b3c7227fac44
JojoZhao1212 <zhaoj@middleton.school.nz>
2021-06-21 12:21:50 +1200

    Create 01_Temperature_Converter.iml

---
+++ 01_Help_GUIv1.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import random
+
+
+class Converter:
+    def __init__(self):
+        print("hello world")
+
+    background_color = "light blue"
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter(root)
+    root.mainloop()
