641182e1d6c197a1a2c627d97c08ac6ebfca4c4d
JojoZhao1212 <zhaoj@middleton.school.nz>
2021-06-22 11:41:03 +1200

    Create 01_Help_GUIv3.py

---
+++ 01_Help_GUIv3.py
@@ -0,0 +1,34 @@
+from tkinter import *
+import random
+
+
+class Convertor:
+    def __init__(self):
+        background_color = "light blue"
+        self.converter_frame = Frame(width=300, height=300,
+                                     bg=background_color, pady=10)
+        self.converter_frame.grid()
+
+        self.temp_converter_label = Label(self.converter_frame,
+                                          text="Temperature Converter",
+                                          font=("Arial", "16", "bold"),
+                                          bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+
+        self.help_button = Button(self.converter_frame, text="Help",
+                                  font=("Arial", "14"),
+                                  padx=10, pady=10, command=self.help)
+        self.help_button.grid(row=1)
+
+    def help(self):
+        print("You asked for help")
+        get_help = Help()
+        get_help.help_text.configure(text="Help text goes here")
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Convertor")
+    something = Convertor()
+    root.mainloop()
