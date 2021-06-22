8c6d027ffc758eb0ec3e5da52b624522c9d609bf
JojoZhao1212 <zhaoj@middleton.school.nz>
2021-06-22 12:00:43 +1200

    Update 01_Help_GUIv3.py

--- 01_Help_GUIv3.py
+++ 01_Help_GUIv3.py
@@ -26,6 +26,29 @@ class Convertor:
         get_help = Help()
         get_help.help_text.configure(text="Help text goes here")
 
+class Help:
+    def __init__(self):
+        background = "orange"
+        self.help_box = Toplevel()
+        self.help_frame = Frame(self.help_box, width=300, bg=background)
+        self.help_frame.grid()
+
+        self.how_heading = Label(self.help_frame, text="Help/Instructions",
+                                 font="arial 10 bold", bg=background)
+        self.how_heading.grid(row=0)
+
+        self.help_text = Label(self.help_frame, text="", justify=LEFT,
+                               width=40, bg=background, wrap=250)
+        self.help_text.grid(row=1)
+
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss",
+                                  width=10, bg=background, font="arial 10 bold",
+                                  command=self.close_help)
+        self.dismiss_btn.grid(row=2, pady=10)
+
+    def close_help(self):
+        self.help_box_destroy()
+
 
 if __name__ == "__main__":
     root = Tk()
