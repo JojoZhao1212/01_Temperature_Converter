b9032a53a358471ba81371dffb3d85022e7986a1
JojoZhao1212 <zhaoj@middleton.school.nz>
2021-06-22 12:08:27 +1200

    Update 01_Help_GUIv1.py

--- 01_Help_GUIv1.py
+++ 01_Help_GUIv1.py
@@ -2,16 +2,21 @@ from tkinter import *
 import random
 
 
-class Converter:
+class Convertor:
     def __init__(self):
         print("hello world")
+        background_color = "light blue"
+        self.converter_frame = Frame(width=300, height=300, bg=background_color)
+        self.converter_frame.grid()
+        self.temp_converter_label = Label(text="Temperature Converter",
+                                          font=("Arial", "16", "bold"),
+                                          bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
 
-    background_color = "light blue"
 
-
-# main routine
 if __name__ == "__main__":
     root = Tk()
-    root.title("Temperature Converter")
-    something = Converter(root)
+    root.title("Temperature Convertor")
+    something = Convertor()
     root.mainloop()
