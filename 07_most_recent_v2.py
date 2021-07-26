da1df41a186a2b716182a429383b107a5f063f5f
JojoZhao1212 <zhaoj@middleton.school.nz>
2021-07-26 12:53:09 +1200

    07

---
+++ 07_most_recent_v2.py
@@ -0,0 +1,25 @@
+# Get data from user and store it in a list, then
+# display the most recent three entries nicely
+# Trial #2 - uses a deque method (no need for reversing ordering)
+
+from collections import deque
+calculations = deque()
+
+# Get five items of Data
+for item in range(0, 5):
+    get_item = input("Enter an item: ")
+
+    # add item to start of 'list'
+    calculations.appendleft(get_item)
+
+
+# Show that everything made it to the list...
+print()
+print("*** The Entire Deque ***")
+print(calculations)
+
+print()
+
+print("*** Most Recent 3 ***")
+for item in range(0, 3):
+    print(calculations[item])
